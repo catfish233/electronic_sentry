fa29dbcabd96b64b7229abc28b5cd5e268428c13
Rong <liu_0818@qq.com>
2023-01-05 12:10:52 +0800

    配置electron启动逻辑

---
+++ my-app/public/electron.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow } = require('electron');
+const isDev = require('electron-is-dev');
+const path = require('path');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 960,
+    height: 600,
+  });
+  mainWindow.loadFile('index.html');
+
+  if (isDev) {
+    mainWindow.loadURL('http://localhost:3000/');
+  } else {
+    mainWindow.loadFile(path.join(__dirname, '/../build/index.html'));
+  }
+
+  mainWindow.webContents.openDevTools();
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+app.on('window-all-closed', function() {
+  if (process.platform !== 'darwin'){
+    app.quit();
+  }
+})
+app.on('activate', function() {
+  if (mainWindow === null) {
+    createWindow();
+  }
+})
